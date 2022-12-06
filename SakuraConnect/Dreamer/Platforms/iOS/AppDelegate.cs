82700cb6e6a867f3fc7d902ee0aad8e62c276d73
Fuyuki Sakura <fuyuki@sakura.live>
2022-12-07 00:29:33 +1000

    add a new dreamer project for a client/mobile client of Sakura Connect

---
+++ SakuraConnect/Dreamer/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Sakura.Live.Connect.Dreamer
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
