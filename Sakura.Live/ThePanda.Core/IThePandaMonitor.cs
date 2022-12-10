e5d70fc7d3e8c92ce2b400588b237dece9a4cf7f
Fuyuki Sakura <fuyuki@sakura.live>
2022-12-10 23:03:05 +1000

    add ThePanda project for dependency registration service as a class library

---
+++ Sakura.Live/ThePanda.Core/IThePandaMonitor.cs
@@ -0,0 +1,43 @@
+﻿using Sakura.Live.ThePanda.Core.Interfaces;
+
+namespace Sakura.Live.ThePanda.Core
+{
+	/// <summary>
+	/// Monitors backend services
+	/// </summary>
+	public interface IThePandaMonitor
+	{
+		/// <summary>
+		/// Gets the running status of the monitor
+		/// </summary>
+		public bool IsRunning { get; }
+
+		/// <summary>
+		/// Adds a service to the monitor list
+		/// </summary>
+		/// <param name="sender">the service that depends on the registered service</param>
+		/// <param name="service">the service to be monitored</param>
+		public void Register(object sender, IAutoStartable service);
+
+		/// <summary>
+		/// Unregisters a parent service and releases all of it's child service 
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <remarks>
+		/// A child service can be relied on multiple parents.
+		/// The child service will only stop until all parents are unregistered
+		/// </remarks>
+		public void Unregister(object sender);
+
+		/// <summary>
+		/// Starts the panda monitor
+		/// </summary>
+		/// <returns></returns>
+		public Task StartAsync();
+
+		/// <summary>
+		/// Stops the panda monitor
+		/// </summary>
+		public void Stop();
+	}
+}
---
+++ Sakura.Live/ThePanda.Core/ThePandaMonitor.cs
@@ -0,0 +1,126 @@
+﻿using Sakura.Live.ThePanda.Core.Interfaces;
+
+namespace Sakura.Live.ThePanda.Core
+{
+	/// <summary>
+    /// Monitors backend services
+    /// </summary>
+    internal class ThePandaMonitor : IThePandaMonitor
+    {
+        /// <summary>
+        /// Sets how many seconds before a service is considered dead without response
+        /// </summary>
+        const int TimeoutIn = 10;
+
+        /// <summary>
+        /// Sets the time between each monitor interval in milliseconds
+        /// </summary>
+        const int MonitorInterval = 2000;
+
+        readonly SemaphoreSlim _serviceLock = new (1,1);
+        readonly Dictionary<IAutoStartable, HashSet<object>> _services = new ();
+
+        ///
+        /// <inheritdoc />
+        ///
+        public bool IsRunning { get; private set; }
+
+        ///
+        /// <inheritdoc />
+        ///
+        public void Register(object sender, IAutoStartable service)
+        {
+            if (_services.ContainsKey(service))
+            {
+                _services[service].Add(sender);
+                return;
+            }
+
+            _services.Add(service, new HashSet<object>(new []
+            {
+                sender
+            }));
+
+            // Starts immediately after register
+            _ = service.StartAsync();
+        }
+
+        ///
+        /// <inheritdoc />
+        ///
+        public void Unregister(object sender)
+        {
+            var parentServiceSets = _services
+                .Where(pair => pair.Value.Contains(sender))
+                .Select(pair => pair.Value)
+                .ToArray();
+            foreach (var parents in parentServiceSets)
+            {
+                parents.Remove(sender);
+            }
+
+            StopServicesNoLongerReferenced();
+        }
+
+        /// <summary>
+        /// Stops all services that are no longer referenced by any other services
+        /// </summary>
+        void StopServicesNoLongerReferenced()
+        {
+            var noLongerRequiredServices = _services
+                .Where(pair => !MonitorRequired(pair))
+                .Select(pair => pair.Key)
+                .ToArray();
+            foreach (var service in noLongerRequiredServices)
+            {
+                service.Status = ServiceStatus.Stopped;
+                service.StopAsync();
+            }
+        }
+
+        ///
+        /// <inheritdoc />
+        ///
+        public async Task StartAsync()
+        {
+            await _serviceLock.WaitAsync();
+
+            IsRunning = true;
+            while (IsRunning)
+            {
+                StopServicesNoLongerReferenced();
+
+                var now = DateTime.Now;
+                var monitoredServices = _services.Where(MonitorRequired)
+                    .Select(pairs => pairs.Key)
+                    .ToArray();
+                foreach (var autoStartable in monitoredServices)
+                {
+                    if (!((now - autoStartable.LastUpdate).TotalSeconds > TimeoutIn)) continue;
+
+                    autoStartable.Status = ServiceStatus.Error;
+                    _ = autoStartable.StartAsync();
+                }
+
+                await Task.Delay(MonitorInterval);
+            }
+            _serviceLock.Release();
+        }
+
+        /// <summary>
+        /// Checks if a service given is dependent by another service
+        /// </summary>
+        /// <param name="servicePair"></param>
+        /// <returns></returns>
+        static bool MonitorRequired(KeyValuePair<IAutoStartable, HashSet<object>> servicePair) 
+            => servicePair.Value.Count >= 1;
+
+        ///
+        /// <inheritdoc />
+        ///
+        public void Stop()
+        {
+            IsRunning = false;
+        }
+    }
+}
