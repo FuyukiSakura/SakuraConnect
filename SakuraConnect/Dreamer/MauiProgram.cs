1f08034368386bd6522c449354ca14024ac64d36
Fuyuki Sakura <fuyuki@sakura.live>
2022-12-11 13:21:29 +1000

    remove the not used weather forecast

--- SakuraConnect/Dreamer/MauiProgram.cs
+++ SakuraConnect/Dreamer/MauiProgram.cs
@@ -1,5 +1,4 @@
-﻿using Sakura.Live.Connect.Dreamer.Data;
-using Sakura.Live.Osc.Core;
+﻿using Sakura.Live.Osc.Core;
 using Sakura.Live.ThePanda.Core;
 
 namespace Sakura.Live.Connect.Dreamer
@@ -22,7 +21,6 @@ namespace Sakura.Live.Connect.Dreamer
 #endif
 	        builder.Services.AddThePanda();
 	        builder.Services.AddOscCore();
-            builder.Services.AddSingleton<WeatherForecastService>();
             
             var app = builder.Build();
             var monitor = app.Services.GetService<IThePandaMonitor>();
