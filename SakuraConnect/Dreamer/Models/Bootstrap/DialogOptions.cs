3b67525e6e3449ce3fe44dde491896c53d495b18
Fuyuki Sakura <fuyuki@sakura.live>
2022-12-11 14:22:11 +1000

    Show confirmation before deleting a sender

---
+++ SakuraConnect/Dreamer/Models/Bootstrap/DialogOptions.cs
@@ -0,0 +1,22 @@
+﻿
+using BlazorBootstrap;
+
+namespace Sakura.Live.Connect.Dreamer.Models.Bootstrap
+{
+    /// <summary>
+    /// A list of default options for <see cref="ConfirmDialogOptions"/>
+    /// </summary>
+    public static class DialogOptions
+    {
+        /// <summary>
+        /// Is shown when user tries to delete a setting
+        /// </summary>
+        public static ConfirmDialogOptions ConfirmDelete = new()
+        {
+            YesButtonText = "Yes",
+            YesButtonColor = ButtonColor.Danger,
+            NoButtonText = "Cancel",
+            NoButtonColor = ButtonColor.Light
+        };
+    }
+}
