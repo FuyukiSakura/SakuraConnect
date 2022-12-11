ad646e06594059e48e4391efcf5462682f2f526b
Fuyuki Sakura <fuyuki@sakura.live>
2022-12-11 16:33:03 +1000

    [Feature] Port Sakura Live Deck OSC Duplicator function (#3)
    * add a new dreamer project for a client/mobile client of Sakura Connect
    
    * add ThePanda project for dependency registration service as a class library
    
    * rename the Dreamer project to `SakuraConnect.Dreamer` to separate class library and product projects
    
    * add Osc Core project
    
    * add `SakuraConnect.Web.Ui` project for shared razor components
    
    * add OSC Duplicator UI
    
    * add app icon
    
    * add Add/Remove OSC sender buttons
    
    * add service config button for OSC receiver service
    
    * fix monitor service keeps stopping a stopped service
    
    * starts monitor service when the app starts
    
    * fix code smells
    
    * remove the not used weather forecast
    
    * editing sender details on the UI has immediate effect now
    
    * fix service start introduces a delay when started after the second time or onwards
    
    * add blazor.bootstrap package
    
    * Show confirmation before deleting a sender
    
    * fix code smells and vulnerability
    
    * layout batter for smaller screens
    
    * add build and deploy badges

---
+++ Sakura.Live/Osc.Core/OscCoreDependencyInjectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Sakura.Live.Osc.Core.Services;
+
+namespace Sakura.Live.Osc.Core
+{
+	public static class OscCoreDependencyInjectionExtensions
+	{
+		/// <summary>
+		/// Add ThePanda services to the specified <see cref="IServiceCollection" />
+		/// </summary>
+		/// <param name="services"></param>
+		/// <returns></returns>
+		public static IServiceCollection AddOscCore(this IServiceCollection services)
+		{
+			services.AddSingleton<OscReceiverService>();
+			services.AddSingleton<OscDuplicateService>();
+			return services;
+		}
+	}
+}
---
+++ Sakura.Live/ThePanda.Core/Interfaces/IAutoStartable.cs
@@ -0,0 +1,50 @@
+﻿
+namespace Sakura.Live.ThePanda.Core.Interfaces
+{
+	/// <summary>
+	/// Represents a service that can be auto started
+	/// </summary>
+	public interface IAutoStartable
+	{
+		/// <summary>
+		/// Is triggered when the service starts
+		/// </summary>
+		event EventHandler Started;
+
+		/// <summary>
+		/// Is triggered when the service stops
+		/// </summary>
+		event EventHandler Stopped;
+        
+		/// <summary>
+		/// Is triggered when the service status changes
+		/// </summary>
+		event EventHandler<ServiceStatus> StatusChanged;
+
+		/// <summary>
+		/// Starts the service
+		/// </summary>
+		/// <returns></returns>
+		Task StartAsync();
+
+		/// <summary>
+		/// Stops the service
+		/// </summary>
+		/// <returns></returns>
+		Task StopAsync();
+
+		/// <summary>
+		/// Gets if the running status of the service
+		/// </summary>
+		ServiceStatus Status { get; set; }
+
+		/// <summary>
+		/// Gets the last update time of the service
+		/// </summary>
+		/// <remarks>
+		/// This can be used to determine if a service is not running properly
+		/// and the service will be restarted when timeout
+		/// </remarks>
+		DateTime LastUpdate { get; }
+	}
+}
