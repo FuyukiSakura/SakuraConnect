51cba4019c36017ceaea8466a70cf8e62c198d38
Fuyuki Sakura <fuyuki@sakura.live>
2023-03-24 18:47:40 +1000

    add voice recognition page

---
+++ SakuraConnect/Dreamer/Pages/Voice.razor.js
@@ -0,0 +1,31 @@
+﻿const SpeechRecognition=window.webkitSpeechRecognition;    
+var recognition = new SpeechRecognition();
+recognition.lang = 'zh-HK';
+
+window.startRecognition = () => {
+    recognition.continuous = true;
+    recognition.interimResults = true;
+    //------------------
+    recognition.onstart=function(){
+        console.log("it work");
+    };
+
+    recognition.onresult=function(event){
+        const last = event.results.length - 1;
+        const res = event.results[last];
+        const text = res[0].transcript;
+        if (res.isFinal) {
+            console.log("processing ....");
+            console.log("You said:"+ text);
+        } 
+        else
+        {
+            console.log("listening:"+text);
+        }
+        DotNet.invokeMethodAsync('SakuraConnect.Dreamer', 'Recognize_OnResult', text)
+              .then(data => {
+                console.log(data);
+              });
+    };
+    recognition.start();
+}
