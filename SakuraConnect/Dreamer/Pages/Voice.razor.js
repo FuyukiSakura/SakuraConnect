5dc007e88499f03c4495128a2b3944627cf8e078
Fuyuki Sakura <fuyuki@sakura.live>
2023-03-26 03:11:10 +1000

    fix recognition stops after a while

--- SakuraConnect/Dreamer/Pages/Voice.razor.js
+++ SakuraConnect/Dreamer/Pages/Voice.razor.js
@@ -1,35 +1,46 @@
-﻿const SpeechRecognition=window.webkitSpeechRecognition;    
-var recognition = new SpeechRecognition();
+﻿let recognizing = false;
+
+const SpeechRecognition=window.webkitSpeechRecognition;    
+let recognition = new SpeechRecognition();
+recognition.continuous = true;
+recognition.interimResults = true;
+//------------------
+recognition.onstart=function(){
+    console.log("it work");
+};
+
+recognition.onresult=function(event){
+    const last = event.results.length - 1;
+    const res = event.results[last];
+    const text = res[0].transcript;
+    if (res.isFinal) {
+        console.log("processing ....");
+        console.log("You said:"+ text);
+    } 
+    else
+    {
+        console.log("listening:"+text);
+    }
+    DotNet.invokeMethodAsync('SakuraConnect.Dreamer', 'Recognize_OnResult', text)
+            .then(data => {
+            console.log(data);
+            });
+};
+
+recognition.onend = function() {
+    console.log("voice recognition terminated");
+    if (recognizing) {
+        recognition.start();
+    }
+};
 
 window.startRecognition = (lang) => {
     recognition.lang = lang;
-    recognition.continuous = true;
-    recognition.interimResults = true;
-    //------------------
-    recognition.onstart=function(){
-        console.log("it work");
-    };
-
-    recognition.onresult=function(event){
-        const last = event.results.length - 1;
-        const res = event.results[last];
-        const text = res[0].transcript;
-        if (res.isFinal) {
-            console.log("processing ....");
-            console.log("You said:"+ text);
-        } 
-        else
-        {
-            console.log("listening:"+text);
-        }
-        DotNet.invokeMethodAsync('SakuraConnect.Dreamer', 'Recognize_OnResult', text)
-              .then(data => {
-                console.log(data);
-              });
-    };
+    recognizing = true;
     recognition.start();
 }
 
 window.stopRecognition = () => {
+    recognizing = false;
     recognition.stop();
 }
