f6fd31e9ca0f61e0dfdb4b06c145fd4dd0a2315a
Fuyuki Sakura <fuyuki@sakura.live>
2023-03-26 17:03:09 +1000

    fix senders list gets duplicated on each load

--- Sakura.Live/Osc.Core/Services/OscDuplicateService.cs
+++ Sakura.Live/Osc.Core/Services/OscDuplicateService.cs
@@ -1,7 +1,10 @@
 ﻿using Sakura.Live.Osc.Core.Events;
 using System.Net.Sockets;
+using System.Text.Json;
+using Sakura.Live.Osc.Core.Models;
 using Sakura.Live.Osc.Core.Settings;
 using Sakura.Live.ThePanda.Core;
+using Sakura.Live.ThePanda.Core.Interfaces;
 
 namespace Sakura.Live.Osc.Core.Services
 {
@@ -14,24 +17,57 @@ namespace Sakura.Live.Osc.Core.Services
         /// Gets or sets a list of senders the duplication service
         /// should send to
         /// </summary>
-        public List<OscSender> Senders { get; set; } = new();
+        public List<OscSender> Senders { get; } = new();
         
         readonly Socket _socket = new (AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
 
+        // Dependencies
+        readonly ISettingsService _settingsSvc;
         readonly IThePandaMonitor _monitorSvc;
         readonly OscReceiverService _receiverService;
 
         /// <summary>
         /// Creates a new instance of <see cref="OscDuplicateService" />
         /// </summary>
+        /// <param name="settingsSvc"></param>
         /// <param name="monitorSvc"></param>
         /// <param name="receiverSvc"></param>
         public OscDuplicateService(
+            ISettingsService settingsSvc,
 	        IThePandaMonitor monitorSvc,
             OscReceiverService receiverSvc)
         {
+            _settingsSvc = settingsSvc;
             _monitorSvc = monitorSvc;
             _receiverService = receiverSvc;
+            LoadSettings();
+        }
+
+        /// <summary>
+        /// Saves settings to the system
+        /// </summary>
+        void SaveSettings()
+        {
+            _settingsSvc.Set(VmcPreferenceKeys.Port, _receiverService.Port.ToString());
+            var jsonSettingString = JsonSerializer.Serialize(Senders);
+            _settingsSvc.Set(VmcPreferenceKeys.Duplicators, jsonSettingString);
+        }
+
+        /// <summary>
+        /// Loads settings from the system
+        /// </summary>
+        void LoadSettings()
+        {
+            _receiverService.Port = int.Parse(_settingsSvc.Get(VmcPreferenceKeys.Port, "39550"));
+            var jsonSettingString = _settingsSvc.Get(VmcPreferenceKeys.Duplicators,
+                OscSender.Default);
+            var settings = JsonSerializer
+                .Deserialize<OscSender[]>(jsonSettingString);
+            if (settings == null)
+            {
+                return;
+            }
+            Senders.AddRange(settings);
         }
 
         /// <summary>
@@ -40,6 +76,7 @@ namespace Sakura.Live.Osc.Core.Services
         /// <returns></returns>
         public async Task StartAsync() 
         {
+            SaveSettings();
             _receiverService.OscReceived += ReceiverService_OnOscReceived;
             _monitorSvc.Register(this, _receiverService);
             await Task.CompletedTask;
