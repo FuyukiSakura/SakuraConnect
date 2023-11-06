321b37811b08d249d5d0a6ea20e0bd2cad5bd9dd
Fuyuki Sakura <fuyuki@sakura.live>
2023-11-06 19:25:39 +1000

    Youtube chat redirection (#19)
    * add endpoint for retreving YouTube livestream ids
    
    * adds control page for redirecting to YouTube
    
    * add cors and use server apis for production

--- SakuraConnect/Client/Program.cs
+++ SakuraConnect/Client/Program.cs
@@ -6,6 +6,10 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+#if DEBUG
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+#else
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://sakuraconnect.azurewebsites.net") });
+#endif
 
 await builder.Build().RunAsync();
---
+++ SakuraConnect/Server/Controllers/YouTubeController.cs
@@ -0,0 +1,37 @@
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace SakuraConnect.Server.Controllers
+{
+    /// <summary>
+    /// Helps with YouTube API
+    /// </summary>
+    [ApiController]
+    [Route("api/[controller]")]
+    public class YouTubeController : ControllerBase
+    {
+        readonly ILogger<YouTubeController> _logger;
+
+        public YouTubeController(ILogger<YouTubeController> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Gets latest live stream of a YouTube Channel
+        /// </summary>
+        /// <param name="channelId"></param>
+        /// <returns></returns>
+        [HttpGet("LiveStream")]
+        public string GetLiveStreamId(string channelId)
+        {
+            var apiKey = Environment.GetEnvironmentVariable("YOUTUBE_API_KEY");
+            var url = $"https://www.googleapis.com/youtube/v3/search?part=snippet&channelId={channelId}&eventType=live&type=video&key={apiKey}";
+            var json = new WebClient().DownloadString(url);
+            dynamic data = JsonConvert.DeserializeObject(json);
+            bool hasLiveStream = data.items.Count > 0;
+            return hasLiveStream ? data.items[0].id.videoId : "NOT_FOUND";
+        }
+    }
+}
