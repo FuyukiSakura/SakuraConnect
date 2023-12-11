b1f544b84cfe06bb32c84512cba1b37965b7e652
Fuyuki Sakura <fuyuki@sakura.live>
2023-12-11 10:59:24 +1000

    make Azure tts errors output to the debug window

--- Sakura.Live/Speech.Core/Services/AzureTextToSpeechService.cs
+++ Sakura.Live/Speech.Core/Services/AzureTextToSpeechService.cs
@@ -72,12 +72,12 @@ namespace Sakura.Live.Speech.Core.Services
                     break;
                 case ResultReason.Canceled:
                     var cancellation = SpeechSynthesisCancellationDetails.FromResult(speechSynthesisResult);
-                    Console.WriteLine($"CANCELED: Reason={cancellation.Reason}");
+                    Debug.WriteLine($"CANCELED: Reason={cancellation.Reason}");
 
                     if (cancellation.Reason == CancellationReason.Error)
                     {
-                        Console.WriteLine($"CANCELED: ErrorCode={cancellation.ErrorCode}");
-                        Console.WriteLine($"CANCELED: ErrorDetails=[{cancellation.ErrorDetails}]");
+                        Debug.WriteLine($"CANCELED: ErrorCode={cancellation.ErrorCode}");
+                        Debug.WriteLine($"CANCELED: ErrorDetails=[{cancellation.ErrorDetails}]");
                         Console.WriteLine($"CANCELED: Did you set the speech resource key and region values?");
                     }
                     break;
